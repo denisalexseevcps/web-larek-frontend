c9cbe01e8891551183e72881d46d1a34b350dba9
Denis Alekseev <denisalexseev@cps.ai>
2024-05-25 20:38:36 +0300

    Описание типов

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,6 +17,11 @@ export interface Order {
     total: number;
 }
 
+export interface OrderRequest extends OrderForm, Contacts {
+    total: number,
+    items: ProductItem['id'][];
+}
+
 export interface Success {
     total : string;
 }
@@ -58,3 +63,47 @@ export interface Error {
 export type ContactsFormErrors = Partial<Record<keyof Contacts, string>>;
 
 export type AddressFormErrors =  Partial<Record<keyof OrderForm, string>>
+
+export interface BasketModel {
+    items: Set<string>;
+    add(id: string): void;
+    remove(id: string): void;
+}
+
+export interface OrderModel extends Partial<OrderRequest> {
+    addAddress(address: string): void;
+    addPhone(phone: string): void;
+    addEmail(email: string): void;
+    addPayment(payment: 'card' | 'cash'): void;
+}
+
+export interface CardComponent extends ProductItem {
+    cardTemplate: HTMLTemplateElement;
+    createCard(): void;
+    openModal(): void;
+}
+
+export interface ModalComponent {
+    close(): void;
+    submit(): void;  
+}
+
+export interface BasketComponent extends ModalComponent {
+   remove(id: string): void;
+}
+
+export interface AddressComponent extends ModalComponent {
+    toggle(payment: 'card' | 'cash'): void;
+    errors: AddressFormErrors;
+    checkAddress(address: string): void;
+}
+
+export interface ContactsComponent extends ModalComponent {
+    errors: ContactsFormErrors;
+    checkEmail(email: string): void;
+    checkPhone(phone: string): void;
+}
+
+export interface SucсessComponent extends Success {
+    close(): void;
+} 
