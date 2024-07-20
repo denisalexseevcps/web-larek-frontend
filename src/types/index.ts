93cd3379d0c2d3791cd2de6251e2e2fb3d5c46b1
Denis Alekseev <denisalexseev@cps.ai>
2024-07-20 22:01:27 +0300

    Основные интерфейсы

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export type PayMetod = 'cash' | 'card';
+
+export interface IProduct {
+    id: string;
+    name: string;
+    price: number | null;
+    description: string;
+    image: string;
+}
+
+export interface IBox {
+    items: string[];
+    total: number;
+}
+
+export interface IOrder {
+    pay: PayMetod;
+    email: string;
+    phone: string;
+    address: string;
+    total: number;
+    item: string[];
+}
+
+export type OrderFrame = Omit<IOrder, 'total'|'item'>;
+
+export interface IResultOrder {
+    id: string;
+    total: number;
+}
