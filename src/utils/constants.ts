1a377cbbdc182a93a6f311eb88605618f9b7c1a1
Denis Alekseev <denisalexseev@cps.ai>
2024-07-26 08:49:50 +0300

    fix_bags

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,17 +1,7 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
-import { CategoryMapping } from '../types';
 
 export const settings = {
 
 };
-
-export const categoryMapping: CategoryMapping = {
-    другое: 'card__category_other',
-    'софт-скил': 'card__category_soft',
-    дополнительное: 'card__category_additional',
-    кнопка: 'card__category_button',
-    'хард-скил': 'card__category_hard',
-  };
-
 
